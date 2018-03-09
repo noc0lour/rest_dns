6b3bc4c9515bd293d5b59215c116c68aa0e0077d
Andrej Rode <mail@andrejro.de>
2018-03-09 16:53:20 +0100

    API: add dyn/ endpoint to update a record based on remote_addr

--- rest_dns/routes.py
+++ rest_dns/routes.py
@@ -4,7 +4,7 @@ from flask_jwt import jwt_required, current_identity
 from .server import app
 from .helper import check_request, check_acl, process_request
 
-import re
+from ipaddress import ip_address
 
 
 @app.route('/api/')
@@ -17,7 +17,7 @@ def api():
 def zone_access(zone, entry):
     if request.method == 'POST':
         zone_request = request.get_json(force=True)
-        zone_acl =  app.config.get('zones').get(zone, None)
+        zone_acl = app.config.get('zones').get(zone, None)
         if zone_acl is None:
             return "No ACL for zone %s found\n" % zone, 404
         # will alter zone_request (fix types)
@@ -40,3 +40,36 @@ def zone_access(zone, entry):
     else:
         return "Not found", 404
 
+@app.route('/api/v1/dyn/<zone>/<entry>', method=['POST'])
+@jwt_required()
+def dyn_update(zone, entry):
+    if request.method == 'POST':
+        zone_request = request.get_json(force=True)
+        zone_acl = app.config.get('zones').get(zone, None)
+        if zone_acl is None:
+            return "No ACL for zone %s found\n" % zone, 404
+        ip_addr = ip_address(request.remote_addr)
+        if ip_addr.version == 4:
+            zone_request["type"] = "A"
+        else:
+            zone_request["type"] = "AAAA"
+        zone_request["target"] = ip_addr.compressed
+        (request_ok, reply) = check_request(zone, entry, zone_request)
+        if not request_ok:
+            return reply, 400
+        if current_identity.username in zone_acl.get('admin_users', ()):
+            process_request(zone, entry, zone_request)
+            return "Request successfully processed\n", 201
+        acls = zone_acl.get('acl')
+        for acl in acls:
+            if check_acl(acl, entry, current_identity, zone_request):
+                if process_request(zone, entry, zone_request, zone_acl):
+                    break
+                else:
+                    return "Syntax Error\n", 400
+        else:
+            return "Access denied\n", 403
+        return "Request successfully processed\n", 201
+
+    else:
+        return "Not found", 404
